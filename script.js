b298140a000a7aeed0d8e89d03d434cea6bfb83c
youareanidiot <135713980+youareagoodperson@users.noreply.github.com>
2023-12-12 13:47:25 +0100

    Update and rename script.js to txt.txt

--- script.js
+++
@@ -1,9 +0,0 @@
-let newWindow = open('/', 'You are An Idiot !', 'width=300,height=300')
-newWindow.focus();
-
-alert(newWindow.location.href); // (*) about:blank, loading hasn't started yet
-
-newWindow.onload = function() {
-  let html = `<div style="font-size:30px">You are an Idiot !</div>`;
-  newWindow.document.body.insertAdjacentHTML('afterbegin', html);
-};
